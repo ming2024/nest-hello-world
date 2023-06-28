bc12a35f3a4604003c4d4c87108f5897abb8b449
ming2024 <111698568+ming2024@users.noreply.github.com>
2023-06-28 20:32:45 +0900

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+    // Enable CORS
+  app.enableCors({
+    origin: 'localhost:3000',
+  });
+  
   await app.listen(3000);
 }
 bootstrap();
